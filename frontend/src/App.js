154e83d022fc7fcbe38bc7e3f4d58649fcd51e02
EuGeN-PYTHON <varlaea2@icloud.com>
2021-11-17 19:20:23 +0300

    add React
    link(frontend and backend)
    add html page-proofs

---
+++ frontend/src/App.js
@@ -0,0 +1,85 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+import UserList from "./components/users"
+import axios from "axios"
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            'users': []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/users').then(response => {
+            const users = response.data
+            this.setState(
+                {
+                    'users': users
+                }
+            )
+        }).catch(error => console.log(error))
+    }
+
+
+    // componentDidMount() {
+    //     const users = [
+    //         {
+    //             'user_name': 'Petr1',
+    //             'first_name': 'Petr',
+    //             'last_name': 'Petrov',
+    //             'email': 'petrov@gmail.com',
+    //         },
+    //         {
+    //             'user_name': 'Ivan1',
+    //             'first_name': 'Ivan',
+    //             'last_name': 'Ivanov',
+    //             'email': 'ivanov@gmail.com',
+    //         }
+    //     ]
+    //     this.setState(
+    //         {'users': users}
+    //     )
+    // }
+
+    render() {
+        return (
+            <div>
+                <nav className="three">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Menu</a></li>
+                    </ul>
+                </nav>
+
+                <UserList users={this.state.users}/>
+
+                <footer className="FOOTER">
+                    Copyright 'Geekbreains'
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default App;
+
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> test.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
